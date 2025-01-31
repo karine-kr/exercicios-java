9c23e7488fcf0c1fb5bb8ad435215374484a1b3c
Karine Kops Ramos <karine.ramos@e-unicred.com.br>
2025-01-31 16:24:12 -0300

    ajuste do desafio3

--- src/Desafio3/MediaEnum.java
+++ src/Desafio3/MediaEnum.java
@@ -1,7 +1,6 @@
 package Desafio3;
 
 public enum MediaEnum {
-//    MEDIA_APROVADO(7), MEDIA_RECUPERACAO(5), MEDIA_REPROVADO(0);
     MEDIA_RECUPERACAO(7), MEDIA_REPROVADO(5);
 
     private float nota;
