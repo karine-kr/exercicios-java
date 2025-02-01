7e6ec7b989cffc787baf701d34a81494591081fc
Karine Kops Ramos <karine.ramos@e-unicred.com.br>
2025-02-01 15:21:54 -0300

    ajuste do desafio4

--- src/Desafio4/Desafio4Main.java
+++ src/Desafio4/Desafio4Main.java
@@ -1,4 +1,50 @@
 package Desafio4;
 
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
 public class Desafio4Main {
+
+    public static void main(String[] args) {
+        Scanner calculadora = new Scanner(System.in);
+
+        System.out.println("Qual operação deseja fazer, digitando um dos seguintes operadores:  + , - , / * ");
+        String operacao = calculadora.nextLine().trim();
+
+        System.out.println("Informe o primeiro número:");
+        Float primeiroNumero = calculadora.nextFloat();
+
+        System.out.println("Informe o segundo número:");
+        Float segundoNumero = calculadora.nextFloat();
+
+        float calculo = 0;
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        switch (operacao) {
+            case "+":
+                calculo = (primeiroNumero + segundoNumero);
+                System.out.println("O valor do cálculo desejado foi de " + df.format(calculo));
+                break;
+            case "/":
+                //validar se valor é zero
+                //se verdadeiro, fazer o cácluclo
+                //senao imprimir mensagem de erro
+                if (segundoNumero != 0){
+                    calculo = (primeiroNumero / segundoNumero);
+                    System.out.println("O valor do cálculo desejado foi de " + df.format(calculo));
+                } else {
+                    System.out.println("Não é possível efetuar divisão informando número zero!");
+                }
+                break;
+            case "-":
+                calculo = (primeiroNumero - segundoNumero);
+                System.out.println("O valor do cálculo desejado foi de " + df.format(calculo));
+                break;
+            case "*":
+                calculo = (primeiroNumero * segundoNumero);
+                System.out.println("O valor do cálculo desejado foi de " + df.format(calculo));
+                break;
+
+        }
+    }
 }
