1f5984b75fa4803dd872fa3729c09d845797e87e
Karine Kops Ramos <karine.ramos@e-unicred.com.br>
2025-02-07 15:26:45 -0300

    ajustes no desafio 14

--- src/Desafio14/Desafio14Main.java
+++ src/Desafio14/Desafio14Main.java
@@ -4,37 +4,38 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Desafio14Main {
-
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        String nomesComRepeticao[] = new String[5];
+        String comNomesDuplicados[] = new String[20];
 
         System.out.print("O sistema irá ler 20 nomes. ");
 
-        for (int i = 0; i < nomesComRepeticao.length; i++) {
+        for (int aux = 0; aux < comNomesDuplicados.length; aux++) {
             System.out.println("Informe o nome: ");
-            nomesComRepeticao[i] = ler.nextLine();
+            comNomesDuplicados[aux] = ler.nextLine();
         }
 
-        String[] nomesSemRepeticao = removeNomesRepetidos(nomesComRepeticao);
-
-        System.out.println("Vetor sem nomes duplicados: "+ Arrays.toString(nomesSemRepeticao));
+        String semNomesDuplicados[] = removerNomesDuplicados(comNomesDuplicados);
+        System.out.println("Os nomes sem duplicação são: " + Arrays.toString(semNomesDuplicados));
     }
 
-    private static String[] removeNomesRepetidos(String[] nomesComRepeticao) {
-        String[] nomesSemRepeticao = new String[nomesComRepeticao.length];
-        int novoTamanho = 0;
+    private static String[] removerNomesDuplicados(String[] comNomesDuplicados){
+        int tamanho = comNomesDuplicados.length;
+        String[]semNomesDuplicados = Arrays.copyOf(comNomesDuplicados, tamanho);
 
-        for (int i = 0; i < nomesComRepeticao.length; i++) {
-            for (int j = 0; j <= novoTamanho; j++) {
-                if (!nomesComRepeticao[i].equals(nomesSemRepeticao[j])) {
-                    nomesSemRepeticao[novoTamanho] = nomesComRepeticao[i];
-                    novoTamanho++;
-                    break;
+        for(int i = 0; i< tamanho; i++){
+            int auxRemocao = i + 1;
+            int removidos = 0;
+            for (int j = i + 1; j < tamanho; j++){
+                if (semNomesDuplicados[j].equals(semNomesDuplicados[i])) {
+                    removidos++;
+                }else {
+                    semNomesDuplicados[auxRemocao++] = semNomesDuplicados[j];
                 }
             }
+            tamanho = tamanho - removidos;
         }
-
-        return Arrays.copyOf(nomesSemRepeticao, novoTamanho);
+        semNomesDuplicados = Arrays.copyOf(semNomesDuplicados, tamanho);
+        return semNomesDuplicados;
     }
 }
